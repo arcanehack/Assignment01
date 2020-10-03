2b6ce9a887e3b9794bec22355d86273fa9b18b80
arcanehack <arcanehack11@gmail.com>
2020-10-03 04:39:48 -0700

    Initial Commit: Basic structure. Added AddingMachine.java.

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * AddingMachine contains total variable
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * getTotal returns 0
+     * @return 0
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * add method
+     * @param value
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * subtract method
+     * @param value
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * toString method
+     * @return a string
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * clear method
+     */
+    public void clear() {
+    }
+}
