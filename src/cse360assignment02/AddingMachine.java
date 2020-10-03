59bd0e417238a1a066f53b6db4fa8f7ced0d9fd9
arcanehack <arcanehack11@gmail.com>
2020-10-03 16:03:00 -0700

    Added extra function to the methods. Added main to interact with those methods and to print out sample problem.

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,47 +2,69 @@ package cse360assignment02;
 
 public class AddingMachine {
     private int total;
+    private String together;
 
     /**
-     * AddingMachine contains total variable
+     * AddingMachine contains total variable and together to print out expressions
      */
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
+        together = "" + total;
     }
 
     /**
-     * getTotal returns 0
-     * @return 0
+     * getTotal returns total
+     * @return total
      */
     public int getTotal () {
-        return 0;
+        return total;
     }
 
     /**
-     * add method
+     * add method takes in a value then adds it to together to be printed, prints method purpose and value
      * @param value
      */
     public void add (int value) {
+        together += " + " + value;
+        System.out.println("myCalulcator.add " + "(" + value + ");");
     }
 
     /**
-     * subtract method
+     * subtract method does the same as add method except subtracts
      * @param value
      */
     public void subtract (int value) {
+        together += " - " + value;
+        System.out.println("myCalulcator.subtract " + "(" + value + ");");
+
     }
 
     /**
-     * toString method
-     * @return a string
+     * toString method returns together, results in the printing of the expression
+     * @return together
      */
+    @Override
     public String toString () {
-        return "";
+        return together;
     }
 
     /**
-     * clear method
+     * clear method unknown purpose
      */
     public void clear() {
+    }
+
+    /**
+     * Main runs the program, inputs the example numbers from assignment page.
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        AddingMachine mach = new AddingMachine();
+        mach.add(4);
+        mach.subtract(2);
+        mach.add(5);
+        System.out.println(mach.toString());
+
     }
 }
